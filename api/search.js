f77bfb11d0c0f2223b38012018be3aa533e8841f
Matthew <matthewodishoo@Matthews-MacBook-Air.local>
2025-10-21 10:03:59 +0200

    changed context to search

--- api/search.js
+++ api/search.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
             },
             context: {
                 page: {
-                    type: "HOMEPAGE",
+                    type: "SEARCH",
                     data: [],
                     location: "https://dy-implementation-demo.vercel.app/",
                     locale: "none",
